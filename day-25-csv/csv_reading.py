a7fd8d909fc92af1b8c928ef3e40a1892636289a
Tom <appleyard.tom@proton.me>
2022-11-16 17:00:31 +0000

    retrieved weather data in an easier way using pandas module

--- day-25-csv/csv_reading.py
+++ day-25-csv/csv_reading.py
@@ -1,17 +1,8 @@
 # Importing modules
-import csv
 import pandas
 
-# with open(file='weather_data.csv') as weather_data:
-#     weather_data_lst = weather_data.readlines()
-#     print(weather_data_lst)
+data = pandas.read_csv('weather_data.csv')
+temps = data['temp']
 
-with open(file='weather_data.csv') as weather_data:
-    data = csv.reader(weather_data)
-    temperatures = []
-    for row in data:
-        print(row)
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
-
-    print(temperatures)
+print(data)
+print(temps)
