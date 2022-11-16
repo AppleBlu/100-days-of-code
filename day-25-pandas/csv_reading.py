146e602d5d7e37690b0dc7c62c789440d8b41c32
Tom <appleyard.tom@proton.me>
2022-11-16 17:18:35 +0000

    updated file

--- day-25-pandas/csv_reading.py
+++ day-25-pandas/csv_reading.py
@@ -2,7 +2,25 @@
 import pandas
 
 data = pandas.read_csv('weather_data.csv')
-temps = data['temp']
 
-print(data)
-print(temps)
+# Collecting data from all columns
+temps = data['temp']
+conditions = data['condition']
+days = data['day']
+# Or a simpler way is:
+# data.temp etc.
+
+data_dict = data.to_dict()
+temp_list = data['temp'].to_list()
+
+# Getting the avg temp
+number_of_temps = len(temp_list)
+all_temps_added = sum(temp_list)
+avg_temp = all_temps_added / number_of_temps
+print(avg_temp)
+
+# Simpler way
+print(data['temp'].mean())
+
+# Getting max temp
+print(data['temp'].max())
