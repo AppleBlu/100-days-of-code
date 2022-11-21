d10829cce8dc8453bf5d7f3a58f8a3305713f89d
Tom <appleyard.tom@proton.me>
2022-11-21 16:20:33 +0000

    Added examples to dict_comp_1.py

---
+++ day-26-lst-dict-comprehensions/dict_comp_1.py
@@ -0,0 +1,9 @@
+"""Dictionary comprehensions
+Dict comp using a list to build the dict
+new_dict = {new_key:new_value for item in list}
+
+Dict comp using a dict to build the dict
+new_dict2 = {new_key:new_value for (key, value) in dict.items()}
+
+Dict comp using a dict to build a dict with an if test
+new_dict3 = {new_key:new_value for (key, value) in dict.items() if test}"""
