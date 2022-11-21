36dab3bac779fe9739c56f8ced5d21215e92924b
Tom <appleyard.tom@proton.me>
2022-11-21 19:55:16 +0000

    dict_comp_challenge.py edited

--- day-26-lst-dict-comprehensions/dict_comp_challenge.py
+++ day-26-lst-dict-comprehensions/dict_comp_challenge.py
@@ -4,4 +4,7 @@ words_from_sentence = sentence.split()
 
 result = {word: len(word) for word in words_from_sentence}
 
+"""Quicker way to do this is:
+result = {word: len(word) for word in sentence.split()}"""
+
 print(result)
