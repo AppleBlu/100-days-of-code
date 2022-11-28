fb0bc477a405ff757b54384743750431a5f83477
Tom <appleyard.tom@proton.me>
2022-11-28 15:08:20 +0000

    flash-card ~ Added comments

--- 100-days-projects/flash-card/flashy.py
+++ 100-days-projects/flash-card/flashy.py
@@ -3,10 +3,12 @@ from tkinter import *
 import pandas
 import random
 
+# Setting constants and variables
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
 to_learn = {}
 
+# Trying to open words_to_learn.csv and if it doesn't exist it will create it
 try:
     data = pandas.read_csv("data/words_to_learn.csv")
 except FileNotFoundError:
@@ -18,6 +20,7 @@ else:
 
 
 def next_card():
+    """Moves on to the next flash card"""
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
@@ -28,12 +31,14 @@ def next_card():
 
 
 def flip_card():
+    """Flips the flash card to show the answer"""
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
 
 def is_known():
+    """Removes the flash card from words to learn if the user clicked the known_button"""
     to_learn.remove(current_card)
     print(len(to_learn))
     data = pandas.DataFrame(to_learn)
@@ -41,21 +46,28 @@ def is_known():
     next_card()
 
 
+# Window creation
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
+# Flipping the card after 3 seconds
 flip_timer = window.after(3000, func=flip_card)
 
+# Canvas creation
 canvas = Canvas(width=800, height=526)
+
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
+
 card_background = canvas.create_image(400, 263, image=card_front_img)
+
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
+# Button creation
 cross_image = PhotoImage(file="images/wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
@@ -64,6 +76,7 @@ check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
+# Calling next card when the program runs
 next_card()
 
 window.mainloop()
