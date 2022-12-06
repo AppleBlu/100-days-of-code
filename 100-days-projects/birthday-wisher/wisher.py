8962cb4b0d245f2b2f39cbaf4e37ae26cd5c4478
AppleBlu <appleyard.tom@proton.me>
2022-12-06 20:58:03 +0000

    wisher.py v1.0.0

--- 100-days-projects/birthday-wisher/wisher.py
+++ 100-days-projects/birthday-wisher/wisher.py
@@ -7,15 +7,18 @@ current_date_time = dt.datetime.now()
 current_month = current_date_time.month
 current_day = current_date_time.day
 
+birthday_person = None
+
 GOOGLE_EMAIL = "codingtest1818@gmail.com"
 PASSWORD = "mqodwegvlghwmwxb"
 
 list_of_letters = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
 
-random_letter = random.choice(list_of_letters)
-
 # 1. Update the birthdays.csv
 birthdays = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = birthdays.to_dict()
+
 days = birthdays['day'].tolist()
 months = birthdays['month'].tolist()
 names = birthdays['name'].tolist()
@@ -28,22 +31,28 @@ print(days)
 print(names)
 print(emails)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+print(birthdays_dict)
+
 for index in range(0, len(names)):
     if current_day == days[index] and current_month == months[index]:
-        persons_birthday = names[index]
+        random_letter = random.choice(list_of_letters)
         with open(file=f"letter_templates/{random_letter}", mode="r") as letter:
             letter_contents = letter.read()
-            letter_with_name = letter_contents.replace("[NAME]", persons_birthday)
-            letter_with_both_names = letter_with_name.replace("Angela", "Tom")
         with open(file=f"letter_templates/{random_letter}", mode="w") as letter:
-            letter.write(letter_with_both_names)
+            letter_with_name = letter_contents.replace("[NAME]", names[index])
+            letter.write(letter_with_name)
         with open(file=f"letter_templates/{random_letter}", mode="r") as letter:
-            finished_letter = letter.read()
-
+            finished_contents = letter.read()
+            print(finished_contents)
+#
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=GOOGLE_EMAIL, password=PASSWORD)
-            connection.sendmail(from_addr=GOOGLE_EMAIL, to_addrs="14kennys@gmail.com",
-                                msg=f"Subject: Happy Birthday!!!\n\n{finished_letter}")
+            connection.sendmail(from_addr=GOOGLE_EMAIL, to_addrs=emails[index],
+                                msg=f"Subject: Happy Birthday {names[index]}!!!\n\n{finished_contents}")
             connection.close()
+#
+
+            reset_letter = letter_contents.replace("test", "[NAME]")
+        with open(file=f"letter_templates/{random_letter}", mode="w") as letter:
+            letter.write(reset_letter)
