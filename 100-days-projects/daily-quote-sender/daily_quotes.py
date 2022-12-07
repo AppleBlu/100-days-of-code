09b6d96a68314fd68db5b1e9a8929e75c544e2df
AppleBlu <appleyard.tom@proton.me>
2022-12-07 12:38:25 +0000

    commit

---
+++ 100-days-projects/daily-quote-sender/daily_quotes.py
@@ -0,0 +1,17 @@
+import random
+import smtplib
+
+with open(file="quotes.txt") as data:
+    quotes = data.readlines()
+
+GOOGLE_EMAIL = "codingtest1818@gmail.com"
+PASSWORD = "mqodwegvlghwmwxb"
+
+random_quote = random.choice(quotes)
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=GOOGLE_EMAIL, password=PASSWORD)
+    connection.sendmail(from_addr=GOOGLE_EMAIL, to_addrs="blu18@protonmail.com",
+                        msg=f"Subject: Daily quote\n\n{random_quote}")
+    connection.close()
