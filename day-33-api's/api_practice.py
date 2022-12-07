008998d2a091b42dfcdb2b0984a40d6e6a63e6eb
AppleBlu <appleyard.tom@proton.me>
2022-12-07 18:51:57 +0000

    Added files

---
+++ day-33-api's/api_practice.py
@@ -0,0 +1,35 @@
+import requests
+
+"""Status codes (X can be any number)
+Wait something is happening: 1XX
+Success you got the data you wanted: 2XX
+You cant get hold of this data: 3XX
+You failed or the data does not exist: 4XX
+The server or something not controlled by you has failed: 5XX"""
+
+# Trying to receive data from this api
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response)
+
+# Checking if we can get the data (You would add more but this is just an example)
+if response.status_code == 404:
+    raise Exception("That response does not exist.")
+elif response.status_code == 401:
+    raise Exception("You are not authorised to access this data.")
+
+# Quicker way of doing this using the request module
+# Will raise the appropriate error for you
+response.raise_for_status()
+
+# Receiving the actual date we want
+data = response.json()
+print(data)
+
+# Getting a specific key
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+# Turning are data into a tuple
+iss_position = (longitude, latitude)
+print(iss_position)
+
