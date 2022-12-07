1ea796957ab0154308897373a45b2c6af47c394d
AppleBlu <appleyard.tom@proton.me>
2022-12-07 22:00:15 +0000

    iss_finder.py v1.0.0

--- 100-days-projects/iss-finder/iss_finder.py
+++ 100-days-projects/iss-finder/iss_finder.py
@@ -1,22 +1,53 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
 MY_LAT = 50.372812
 MY_LONG = -4.145966
+my_google_email = "codingtest1818@gmail.com"
+password = 'mqodwegvlghwmwxb'
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0
-}
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
+def is_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
-print(sunset)
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-time_now = datetime.now()
-print(time_now)
+    # Your position is within +5 or -5 degrees of the ISS position.
+
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    sun_response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    sun_response.raise_for_status()
+    sun_data = sun_response.json()
+    sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
+    if sunset < time_now.hour < sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_night() and is_overhead():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_google_email, password=password)
+            connection.sendmail(from_addr=my_google_email,
+                                to_addrs="Blu18@protonmail.com",
+                                msg="Subject:ISS\n\nLOOK UP!!!👆")
+            connection.close()
