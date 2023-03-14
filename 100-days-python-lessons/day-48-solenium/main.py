b5332bf0a3dff890635d41d7085b4488e6254b42
AppleBlu <appleyard.tom@proton.me>
2023-03-14 14:30:57 +0000

    day-48 v0.5.0

--- 100-days-python-lessons/day-48-solenium/main.py
+++ 100-days-python-lessons/day-48-solenium/main.py
@@ -9,7 +9,30 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options)
 
 # Pulling up the website below (amazon item for sale)
-driver.get(url="https://www.amazon.co.uk/Stuffed-Animal-Plush-Cuddly-Toddlers/dp/B09N1DZ6KQ/ref=sr_1_1_sspa?keywords=cow+teddy&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1")
+driver.get(url="https://www.amazon.co.uk")
+
+# Accepting the cookies
+# Finding the cookie button
+cookie_button = driver.find_element(by="xpath", value='//*[@id="sp-cc-accept"]')
+
+# Clicking the button
+cookie_button.click()
+
+# Navigating to the item we want the price for
+# Finding the search bar
+search = driver.find_element(by="xpath", value='//*[@id="twotabsearchtextbox"]')
+
+# Typing in cow teddy into the search bar and clicking on the submit button
+search.send_keys("cow teddy")
+search.submit()
+
+
+# Finding the item we want
+item = driver.find_element(by="xpath", value='//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div'
+                                             '/div/div/div[2]/div[1]/h2/a/span')
+
+# Clicking on the item we want
+item.click()
 
 # Getting the price of the amazon item
 price_pound = driver.find_element(by="class name", value="a-price-whole")
@@ -21,7 +44,8 @@ print(f"£{price_pound.text}.{price_pence.text}")
 # Getting the price_pence using another method (xpath)
 # To do this I right-clicked on the element after inspecting and clicked copy > copy xpath
 # I used single quotes because the xpath already had double quotes
-price_pence_2 = driver.find_element(by="xpath", value='//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[3]')
+price_pence_2 = driver.find_element(by="xpath",
+                                    value='//*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span[2]/span[2]/span[3]')
 print(price_pence_2.text)
 
 # Closing the current tab
