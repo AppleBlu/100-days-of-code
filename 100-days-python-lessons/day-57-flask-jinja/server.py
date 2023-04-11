c23dcc07327641e572b4ebab0447338d13463c15
AppleBlu <appleyard.tom@proton.me>
2023-04-11 19:23:33 +0100

    day-57 v0.0.3

--- 100-days-python-lessons/day-57-flask-jinja/server.py
+++ 100-days-python-lessons/day-57-flask-jinja/server.py
@@ -7,6 +7,7 @@ import requests
 GENDER_API = "https://api.genderize.io?name="
 AGE_API = "https://api.agify.io?name="
 
+# Creating the flask app
 app = Flask(__name__)
 
 # Getting the current date
@@ -15,26 +16,36 @@ current_date = datetime.now()
 current_year = str(current_date)[0: 4]
 
 
+# When on the home route of the website run this function
 @app.route("/")
 def home(name=None):
-    # Adding a kwarg to to take in python code and insert it into the html file (template being rendered)
+    # Adding a kwarg to take in python code and insert it into the html file (template being rendered)
     return render_template("index.html", name=name, year=current_year)
 
 
+# When on the /name route of the website run this function
 @app.route("/<string:name>")
+# Function takes in the name (string) that is typed into the url after the /
 def guess_age_and_gender(name):
+    # Using the gender api with the name passed into the url
     response = requests.get(url=f"{GENDER_API}{name}")
+    # Checking for any errors involving the api call
     response.raise_for_status()
+    # Collecting the data in a json format from the api call
     gender_data = response.json()
+    # Selecting the specific piece of data that we require
     gender = gender_data["gender"]
 
+    # Repeating the process for the age api
     response = requests.get(url=f"{AGE_API}{name}")
     response.raise_for_status()
     age_data = response.json()
     age = age_data["age"]
 
+    # Rendering the template and passing in data
     return render_template("name_calculator.html", name=name, gender=gender, age=age)
 
 
+# If name is the same as __main__ then run the app in debug mode
 if __name__ == "__main__":
     app.run(debug=True)
